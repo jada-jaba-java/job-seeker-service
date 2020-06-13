a55387824cf3cdf407bf8e27e01f5e861d97f590
Mikhail Vyshinskiy <12588515+Squawk7777@users.noreply.github.com>
2020-06-13 16:17:09 +0300

    fixed temporary test

--- src/test/java/com/jadajabajava/JobSeekerServiceTest.java
+++ src/test/java/com/jadajabajava/JobSeekerServiceTest.java
@@ -1,11 +1,12 @@
 package com.jadajabajava;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class JobSeekerServiceTest {
 
     @Test
     public void someTest() {
-
+        Assertions.assertTrue(true);
     }
 }
