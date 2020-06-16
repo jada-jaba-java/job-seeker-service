53fd88cd239eec1dd729d886367ccc9860be9d25
Mikhail V <12588515+Squawk7777@users.noreply.github.com>
2020-06-17 01:23:32 +0300

    #15 update for batching support

--- src/main/java/com/jadajabajava/entities/AbstractEntity.java
+++ src/main/java/com/jadajabajava/entities/AbstractEntity.java
@@ -11,6 +11,6 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public abstract class AbstractEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 }
