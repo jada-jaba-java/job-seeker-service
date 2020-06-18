0fe12dec9d5e9cf7c5f5f2980e573369a65a2226
Mikhail V <12588515+Squawk7777@users.noreply.github.com>
2020-06-18 17:40:08 +0300

    #15 code cleanup

--- src/main/java/com/jadajabajava/exceptions/CommonServiceException.java
+++ src/main/java/com/jadajabajava/exceptions/CommonServiceException.java
@@ -1,9 +1,6 @@
 package com.jadajabajava.exceptions;
 
 public class CommonServiceException extends Exception {
-    public CommonServiceException(String message) {
-        super(message);
-    }
 
     public CommonServiceException(String message, Throwable cause) {
         super(message, cause);
